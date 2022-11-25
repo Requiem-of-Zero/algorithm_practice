0dfbfcde6829fe8ccff4d641417d47d964342f11
requiem-of-zero <swonggzz@gmail.com>
2022-11-24 22:39:58 -0800

    Added reorder list algorithm

--- neetcode/linkedLists.js
+++ neetcode/linkedLists.js
@@ -19,3 +19,53 @@ var reverseList = function (head, prev = null) {
   }
   return prev;
 };
+
+// Reorder List 1 -> 2 -> 3 -> 4 to 1 -> 4 -> 2 -> 3
+
+var reorderList = function (head) {
+  const mid = getMid(head);
+  const reverseListFromMid = reverseList(mid);
+
+  reorder(head, reverseListFromMid);
+};
+
+  // Helpers
+const getMid = (head) => {
+  let [slow, fast] = [head, head];
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
+
+const reverseList = (head) => {
+  let [prev, curr, next] = [null, head, null];
+
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+
+const reorder = (list1, list2) => {
+  let [first, next, second] = [list1, null, list2];
+
+  while (second.next) {
+    next = first.next;
+    first.next = second;
+    first = next;
+
+    next = second.next;
+    second.next = first;
+    second = next;
+  }
+};
+
