160fb1fd28f4a8ea2f8437c8c2ef81c5dd8bd34f
requiem-of-zero <swonggzz@gmail.com>
2022-11-28 21:17:39 -0800

    Added max depth recursive algorithm

--- neetcode/trees.js
+++ neetcode/trees.js
@@ -1,5 +1,5 @@
 // Invert Binary Tree LeetCode #266
-  // O(n) time O(1) space
+  // O(n) time O(1) space *recursive*
 var invertTree = function (root) {
   if (!root) return null;
 
@@ -12,4 +12,11 @@ var invertTree = function (root) {
   return root;
 };
 
-
+// Maximum Depth in Tree LeetCode #104
+  // O(n) time O(1) space *recursive*
+var maxDepth = function (root) {
+  if (!root) return 0;
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return 1 + Math.max(left, right);
+};
