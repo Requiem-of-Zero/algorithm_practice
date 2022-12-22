63681f1498004c81c9a9e9980d012f5d49551772
requiem-of-zero <theesamwong@gmail.com>
2022-12-21 21:22:15 -0800

    Added new algos

--- neetcode/graphs.js
+++ neetcode/graphs.js
@@ -182,3 +182,42 @@ var getNeighbors = (row, rows, col, cols) =>
 
 const getMatrix = (rows, cols) =>
   new Array(rows).fill().map(() => new Array(cols).fill(false));
+
+  const createGraph = (numCourses, edges) => {
+    const graph = Array.from({ length: numCourses }, () => []);
+
+    for (let edge of edges) {
+      let [a, b] = edge;
+
+      if (!(a in graph)) graph[a] = [];
+      if (!(b in graph)) graph[b] = [];
+      graph[a].push(b);
+    }
+
+    return graph;
+  };
+
+  // * Course Schedule can finish neetcode
+  var canFinish = function (numCourses, prerequisites) {
+    const graph = createGraph(numCourses, prerequisites);
+    let [seen, seeing] = [new Set(), new Set()];
+
+    function explore(course) {
+      if (seen.has(course)) return true;
+      if (seeing.has(course)) return false;
+
+      seeing.add(course);
+      for (let neighbor of graph[course]) {
+        if (!explore(neighbor)) return false;
+      }
+
+      seen.add(course);
+      seeing.delete(course);
+      return true;
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+      if (!explore(i)) return false;
+    }
+    return true;
+  };
