d21398f7c9ce3ca870ad0f9be8b2299040262a4f
requiem-of-zero <theesamwong@gmail.com>
2022-12-22 23:02:02 -0800

    Added final value of variable solution leetcode 2011

--- leetcode-solutions/solutions.js
+++ leetcode-solutions/solutions.js
@@ -377,3 +377,18 @@ var buildArray = function (nums) {
 
   return resultArr;
 };
+
+// * #2011 Final Value of Variable After Performing Operations
+var finalValueAfterOperations = function (operations) {
+  let result = 0;
+
+  for (const operation of operations) {
+    if (operation === "X++" || operation === "++X") {
+      result++;
+    } else {
+      result--;
+    }
+  }
+
+  return result;
+};
