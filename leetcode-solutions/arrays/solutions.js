6881476ee280a31929c1e4fcf75eb3c524066cf5
requiem-of-zero <swonggzz@gmail.com>
2023-01-06 21:33:01 -0800

    Added sort people algorithm

--- leetcode-solutions/arrays/solutions.js
+++ leetcode-solutions/arrays/solutions.js
@@ -10,3 +10,23 @@ var minMovesToSeat = function (seats, students) {
   }
   return count;
 };
+
+// * #2418. Sort the People
+// Time O(n log n) Space O(n)
+var sortPeople = function (names, heights) {
+  if (names.length === 1) return names;
+  let [nameHeightMatrix, result] = [[], []];
+
+  for (let i = 0; i < names.length; i++) {
+    nameHeightMatrix.push([names[i], heights[i]]);
+  }
+
+  nameHeightMatrix.sort((a, b) => b[1] - a[1]);
+
+  for (let [name, height] of nameHeightMatrix) {
+    result.push(name);
+  }
+
+  return result;
+};
+
