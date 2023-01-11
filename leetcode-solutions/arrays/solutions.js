66a27182ea6d577be2a35ab73a9174a38b3cbde2
requiem-of-zero <swonggzz@gmail.com>
2023-01-10 23:08:35 -0800

    Added maximum units on a truck solution

--- leetcode-solutions/arrays/solutions.js
+++ leetcode-solutions/arrays/solutions.js
@@ -525,3 +525,21 @@ var replaceElements = function (arr) {
 
 //   return res;
 // };
+
+// * #1710. Maximum Units on a Truck
+// Time O(nlogn)
+// Space O(1)
+var maximumUnits = function (boxTypes, truckSize) {
+  boxTypes.sort((a, b) => b[1] - a[1]);
+
+  let ans = 0;
+  for (const [count, units] of boxTypes) {
+    const takeBoxes = Math.min(count, truckSize);
+    ans += takeBoxes * units;
+    truckSize -= takeBoxes;
+
+    if (truckSize === 0) break;
+  }
+
+  return ans;
+};
