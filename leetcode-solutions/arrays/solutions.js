962dd7c35608e52f045a06a4def3cddab3529b09
requiem-of-zero <swonggzz@gmail.com>
2023-01-17 23:31:03 -0800

    Added pascals triangle solution

--- leetcode-solutions/arrays/solutions.js
+++ leetcode-solutions/arrays/solutions.js
@@ -902,3 +902,24 @@ const sameCharacters = (word1, word2) => {
 
   return [...set1].every((char) => set2.has(char));
 };
+
+// * #118. Pascal's Triangle
+// Time O(n ^ 2)
+// Space O(n ^ 2)
+var generate = function (numRows) {
+  if (numRows === 0) return [];
+  if (numRows === 1) return [[1]];
+  let result = [];
+  for (let row = 1; row <= numRows; row++) {
+    let arr = [];
+    for (let col = 0; col < row; col++) {
+      if (col === 0 || col === row - 1) {
+        arr.push(1);
+      } else {
+        arr.push(result[row - 2][col - 1] + result[row - 2][col]);
+      }
+    }
+    result.push(arr);
+  }
+  return result;
+};
