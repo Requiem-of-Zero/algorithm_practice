a79f0de0c68d99fd266c4d515b186ccd0e326a61
requiem-of-zero <theesamwong@gmail.com>
2023-01-27 23:17:12 -0800

    Added count special quadruplets solution

--- leetcode-solutions/arrays/solutions.js
+++ leetcode-solutions/arrays/solutions.js
@@ -1848,3 +1848,22 @@ var findRelativeRanks = function (score) {
 
   return output;
 };
+
+// * #1995. Count Special Quadruplets
+// Time O(n ^ 4)
+// Space O(1)
+var countQuadruplets = function (nums) {
+  let count = 0;
+
+  for (let a = 0; a < nums.length; a++) {
+    for (let b = a + 1; b < nums.length; b++) {
+      for (let c = b + 1; c < nums.length; c++) {
+        for (let d = c + 1; d < nums.length; d++) {
+          if (nums[a] + nums[b] + nums[c] === nums[d] && a < b < c < d) count++;
+        }
+      }
+    }
+  }
+
+  return count;
+};
