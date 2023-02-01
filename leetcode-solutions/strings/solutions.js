bdbdb5634db2bea5ea8c3807b81ba84437ce6cbd
requiem-of-zero <theesamwong@gmail.com>
2023-01-31 21:17:37 -0800

    Added count asterisks solution

--- leetcode-solutions/strings/solutions.js
+++ leetcode-solutions/strings/solutions.js
@@ -18,3 +18,25 @@ var minPartitions = function (n) {
 
   return count;
 };
+
+// * #2315. Count Asterisks
+// Time O(n)
+// Space O(1)
+var countAsterisks = function (s) {
+  let [pipeCount, asteriskCount, pair] = [0, 0, false];
+
+  for (const char of s) {
+    if (char === "|") {
+      pipeCount++;
+    } else if (char === "*" && pair === true) {
+      asteriskCount++;
+    } else if (char === "*" && pipeCount === 0) {
+      asteriskCount++;
+    }
+
+    if (pipeCount % 2 === 0) pair = true;
+    else pair = false;
+  }
+
+  return asteriskCount;
+};
