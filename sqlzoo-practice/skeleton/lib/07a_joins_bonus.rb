0bc157e582df260707a073a925ff50569dc53692
requiem-of-zero <swonggzz@gmail.com>
2023-02-06 21:55:15 -0800

    Completed joins bonus sqlzoo practice

--- sqlzoo-practice/skeleton/lib/07a_joins_bonus.rb
+++ sqlzoo-practice/skeleton/lib/07a_joins_bonus.rb
@@ -107,6 +107,18 @@ def best_value
   # pence. Find the good value albums - show the title, the price and the number
   # of tracks.
   execute(<<-SQL)
+    SELECT
+      albums.title,
+      albums.price,
+      COUNT(tracks.*)
+    FROM
+      albums
+    JOIN
+      tracks ON albums.asin = tracks.album
+    GROUP BY
+      albums.asin
+    HAVING
+      (albums.price / COUNT(tracks.*)) < 0.5
   SQL
 end
 
@@ -115,6 +127,12 @@ def top_track_counts
   # tracks. List the top 10 albums. Select both the album title and the track
   # count, and order by both track count and title (descending).
   execute(<<-SQL)
+    SELECT albums.title, COUNT(tracks.*)
+    FROM albums
+    JOIN tracks ON albums.asin = tracks.album
+    GROUP BY albums.asin
+    ORDER BY COUNT(tracks.*) DESC, albums.title DESC
+    LIMIT 10
   SQL
 end
 
@@ -122,6 +140,13 @@ def rock_superstars
   # Select the artist who has recorded the most rock albums, as well as the
   # number of albums. HINT: use LIKE '%Rock%' in your query.
   execute(<<-SQL)
+    SELECT albums.artist, COUNT(DISTINCT albums.asin)
+    FROM albums
+    JOIN styles ON albums.asin = styles.album
+    WHERE styles.style LIKE '%Rock%'
+    GROUP BY albums.artist
+    ORDER BY COUNT(DISTINCT albums.asin) DESC
+    LIMIT 1
   SQL
 end
 
@@ -134,5 +159,29 @@ def expensive_tastes
   # subquery. Next, JOIN the styles table to this result and use aggregates to
   # determine the average price per track.
   execute(<<-SQL)
+    SELECT
+      styles.style,
+      (SUM(album_track_counts.price) / SUM(album_track_counts.count))
+    FROM
+      styles
+    JOIN (
+      SELECT
+        albums.*,
+        COUNT(tracks.*) AS count
+      FROM
+        albums
+      JOIN
+        tracks ON albums.asin = tracks.album
+      WHERE
+        albums.price IS NOT NULL
+      GROUP BY
+        albums.asin
+      ) AS album_track_counts ON styles.album = album_track_counts.asin
+    GROUP BY
+      styles.style
+    ORDER BY
+      (SUM(album_track_counts.price) / SUM(album_track_counts.count)) DESC,
+      styles.style ASC
+    LIMIT 5
   SQL
 end
