989c5a4e41b4522e4646bc822b11cd5e2ec97070
requiem-of-zero <swonggzz@gmail.com>
2023-02-07 22:48:28 -0800

    Added customer who visisted but didnt make any transactions

--- leetcode-solutions/sql/easy.sql
+++ leetcode-solutions/sql/easy.sql
@@ -171,5 +171,19 @@ group by
 having
   SUM(transactions.amount) > 10000
 
--- * 
--- 
+-- * 1581.Customer Who Visited but Did Not Make Any Transactions
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+select
+  customer_id,
+  count(visit_id) as count_no_trans
+from
+  Visits
+where
+  visit_id not in (
+    select
+      visit_id
+    from
+      Transactions
+  )
+group by
+  customer_id
