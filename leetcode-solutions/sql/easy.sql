fa557f3a2bcd4988282c8cb60587631cf98ff352
requiem-of-zero <theesamwong@gmail.com>
2023-02-08 22:21:47 -0800

    Added actors and directors who cooperated at least 3 times

--- leetcode-solutions/sql/easy.sql
+++ leetcode-solutions/sql/easy.sql
@@ -300,3 +300,16 @@ group by
   user_id
 order by
   user_id asc
+
+-- * 
+-- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+select
+  actor_id,
+  director_id
+from
+  actordirector
+group by
+  actor_id,
+  director_id
+having
+  count(timestamp) >= 3
