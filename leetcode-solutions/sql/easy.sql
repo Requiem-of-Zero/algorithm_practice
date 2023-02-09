6e700f20ed2f56e8812f270af6a5b3a204379f95
requiem-of-zero <theesamwong@gmail.com>
2023-02-08 22:27:50 -0800

    Added calculate special bonus solution

--- leetcode-solutions/sql/easy.sql
+++ leetcode-solutions/sql/easy.sql
@@ -405,3 +405,14 @@ group by
 order by
   travelled_distance desc,
   name asc
+
+-- * 1873.Calculate Special Bonus
+-- https://leetcode.com/problems/calculate-special-bonus/
+select
+  employee_id,
+  salary * (employee_id % 2) * (name not like 'M%') as bonus
+from
+  employees
+order by
+  employee_id
+
