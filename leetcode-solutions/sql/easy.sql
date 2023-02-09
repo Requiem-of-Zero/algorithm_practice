a85dd67fb59521bdf6287a2db81f1759a232bfed
requiem-of-zero <theesamwong@gmail.com>
2023-02-08 22:28:28 -0800

    Added user activity for the past 30 days I solution

--- leetcode-solutions/sql/easy.sql
+++ leetcode-solutions/sql/easy.sql
@@ -416,3 +416,15 @@ from
 order by
   employee_id
 
+-- * 1141.User Activity for the Past 30 Days I
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/
+select
+  activity_date as day,
+  count(distinct user_id) active_users
+from
+  activity
+where
+  activity_date > cast('2019-06-27' as date)
+  and activity_date <= cast('2019-07-27' as date)
+group by
+  activity_date
