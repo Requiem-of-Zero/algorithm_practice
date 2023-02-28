9907cf531db07a29dc8f2b03653eb54321315033
requiem-of-zero <theesamwong@gmail.com>
2023-02-27 21:27:37 -0800

    Added greatest english letter in upper and lower case solution

--- leetcode-solutions/strings/solutions.js
+++ leetcode-solutions/strings/solutions.js
@@ -194,3 +194,20 @@ var fizzBuzz = function (n) {
 
   return output;
 };
+
+// * #2309. Greatest English Letter in Upper and Lower Case
+// Time O(n)
+// Space O(n)
+var greatestLetter = function (s) {
+  let set = new Set(s.split(""));
+  // ASCII(A-Z, a-z)=(65-90, 97-122).
+  for (let i = 90; i >= 65; i--) {
+    if (
+      set.has(String.fromCharCode(i)) &&
+      set.has(String.fromCharCode(i + 32))
+    ) {
+      return String.fromCharCode(i);
+    }
+  }
+  return "";
+};
