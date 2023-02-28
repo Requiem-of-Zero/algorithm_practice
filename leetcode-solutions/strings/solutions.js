0a5f8fb852353bedfdd5372b0f6d066beddb9e8b
requiem-of-zero <theesamwong@gmail.com>
2023-02-27 21:26:37 -0800

    Added fizz buzz solution

--- leetcode-solutions/strings/solutions.js
+++ leetcode-solutions/strings/solutions.js
@@ -173,3 +173,24 @@ var freqAlphabets = function (s) {
 
   return output;
 };
+
+// * #412. Fizz Buzz
+// Time O(n)
+// Space O(n)
+var fizzBuzz = function (n) {
+  const output = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(`${i}`);
+    }
+  }
+
+  return output;
+};
