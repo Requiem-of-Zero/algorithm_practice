4d2294fa25860054f3521b90e21e68af64047515
requiem-of-zero <theesamwong@gmail.com>
2023-02-27 21:25:57 -0800

    Added decrypt string from alphabet to interget mapping solution

--- leetcode-solutions/strings/solutions.js
+++ leetcode-solutions/strings/solutions.js
@@ -123,3 +123,53 @@ var squareIsWhite = function (coordinates) {
     return true;
   return false;
 };
+
+// * #1309. Decrypt String from Alphabet to Integer Mapping
+// Time O(n)
+// Space O(n)
+var freqAlphabets = function (s) {
+  const map = {
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+    5: "e",
+    6: "f",
+    7: "g",
+    8: "h",
+    9: "i",
+    10: "j",
+    11: "k",
+    12: "l",
+    13: "m",
+    14: "n",
+    15: "o",
+    16: "p",
+    17: "q",
+    18: "r",
+    19: "s",
+    20: "t",
+    21: "u",
+    22: "v",
+    23: "w",
+    24: "x",
+    25: "y",
+    26: "z",
+  };
+  const stack = [];
+  for (const char of s) {
+    if (char !== "#") {
+      stack.push(char);
+      continue;
+    }
+    const digit = stack.pop();
+    stack.push(map[stack.pop() + digit]);
+  }
+
+  let output = "";
+  for (const char of stack) {
+    output += char <= "9" ? map[char] : char;
+  }
+
+  return output;
+};
