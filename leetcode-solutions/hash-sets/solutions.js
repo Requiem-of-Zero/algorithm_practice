b71a6f619bafc3c29bd8bd774ad8d501219b0dc2
requiem-of-zero <swonggzz@gmail.com>
2023-03-07 23:05:36 -0800

    Added roman to integer solution

--- leetcode-solutions/hash-sets/solutions.js
+++ leetcode-solutions/hash-sets/solutions.js
@@ -51,3 +51,29 @@ var maxLengthBetweenEqualCharacters = function (s) {
 
   return largest;
 };
+
+// * #13. Roman to Integer
+// Time O(n)
+// Space O(1)
+symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+var romanToInt = function (s) {
+  value = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    // is the current roman numeral less than the next roman numeral?
+    //  if it is then subtract the roman value from the output value
+    //  otherwise add the value to the output value
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+  return value;
+};
