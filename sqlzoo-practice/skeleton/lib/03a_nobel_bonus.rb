0d1b07a3eac07495d4a45f92d759df38ebbd6e9f
requiem-of-zero <swonggzz@gmail.com>
2023-03-16 22:31:34 -0700

    Completed 3rd bonus run

--- sqlzoo-practice/skeleton/lib/03a_nobel_bonus.rb
+++ sqlzoo-practice/skeleton/lib/03a_nobel_bonus.rb
@@ -11,6 +11,8 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
-  
+    select distinct yr
+    from nobels
+    where yr not in (select yr from nobels where subject = 'Chemistry') and subject = 'Physics'
   SQL
 end
